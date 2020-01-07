60ca893e5abe7e9eac78b277f58bb24b6eca4010
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2020-01-06 20:47:57 -0800

    Add proxy and deploy URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri:'http://localhost:3313'
+    uri:'https://calm-anchorage-77182.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
