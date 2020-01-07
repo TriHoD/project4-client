881d600cf6e15f970d08a98a731f03406010db4c
Ho Dac Tri <miugypig@Hos-MacBook-Pro.local>
2020-01-06 20:01:09 -0800

    Client side ready to deploy

---
+++ src/components/deletebutton/deleteButton.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
+import { Button, Confirm, Icon } from 'semantic-ui-react';
+
+import { FETCH_POSTS_QUERY } from '../../util/graphql';
+import Mypopup from '../../util/myPopup' 
+
+function DeleteButton({ postId, commentId, callback }) {
+    const [confirmOpen, setConfirmOpen] = useState(false);
+
+    const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+    const [deletePostOrMutation] = useMutation(mutation, {
+        update(proxy) {
+            setConfirmOpen(false);
+            // remove post form cache
+            if (!commentId) {
+                const data = proxy.readQuery({
+                    query: FETCH_POSTS_QUERY
+                });
+                const resPosts = data.getPosts.filter(p => p.id !== postId);
+                proxy.writeQuery({
+                    query: FETCH_POSTS_QUERY,
+                    data: { getPosts: [...resPosts] }
+                });
+            }
+            if(callback) callback();
+        },
+        variables: { postId,  commentId},
+        
+    }); 
+    return (
+        <>
+        <Mypopup content ={commentId ? 'Delete comment' : 'Delete post'}>
+                <Button
+                    as="div"
+                    color="orange"
+                    floated="right"
+                    onClick={() => setConfirmOpen(true)}
+                >
+                    <Icon name="delete" style={{ margin: 0 }} />
+                </Button>
+            </Mypopup>
+            <Confirm
+                open={confirmOpen}
+                onCancel={() => setConfirmOpen(false)}
+                onConfirm={deletePostOrMutation}
+            />
+        </>
+    );
+};
+
+const DELETE_POST_MUTATION = gql`
+    mutation deletePost($postId: ID!) {
+        deletePost(postId: $postId)
+    }
+`;
+
+const DELETE_COMMENT_MUTATION = gql`
+    mutation deleteComment($postId: ID!, $commentId: ID!) {
+        deleteComment(postId: $postId, commentId: $commentId) {
+            id
+            comments {
+                id
+                username
+                createdAt
+                body
+            }
+            commentCount
+        }
+    }
+`;
+
+export default DeleteButton;
---
+++ src/components/home/home.js
@@ -0,0 +1,50 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Grid, Transition } from 'semantic-ui-react';
+
+import { AuthContext }  from '../../context/auth';
+import PostForm from '../postform/postform';
+import PostCard from '../postcard/postcard';
+import { FETCH_POSTS_QUERY } from '../../util/graphql';
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+  useEffect(() => {
+    if (data) {
+      setPosts(data.getPosts);
+    }
+  }, [data]);
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+          <h1>Recent Posts</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {user &&(
+          <Grid.Column>
+              <PostForm />
+          </Grid.Column>
+        )}
+        { 
+          loading ? (
+              <h1>Loading posts..</h1>
+          ) : (
+            <Transition.Group>
+            {posts &&
+              posts.map((post) => (
+                <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+                  <PostCard post={post} />
+                </Grid.Column>
+              ))}
+          </Transition.Group>
+          )
+        }
+      </Grid.Row>
+    </Grid>
+  )
+}
+
+export default Home;
